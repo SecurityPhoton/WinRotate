0974f9b3103b3aacf99a5614557c4fe4ec364304
Pontarr <pontarr@gmail.com>
2015-07-02 11:46:51 +0300

    initial project stamp

---
+++ WinRotate/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace WinRotate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button11 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(369, 8);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(205, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 71);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(562, 127);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Manually clean files in folders";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Clean Folders List";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(371, 42);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(23, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(394, 42);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(23, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(417, 42);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(23, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(440, 42);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(23, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "5";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(463, 42);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(23, 23);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "7";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(486, 42);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(34, 23);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "30";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(526, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "days ago";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "older then";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 204);
+            this.textBox3.MaxLength = 32164;
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(562, 87);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(231, 6);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(130, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Set current date";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 301);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(586, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "Victor Bidnenko 2015";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 31);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 34);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "Run this every";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "",
+            "hour",
+            "day",
+            "2 days",
+            "5 days",
+            "week"});
+            this.comboBox1.Location = new System.Drawing.Point(93, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(55, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(93, 54);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Repeat";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(160, 34);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown1.TabIndex = 15;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(160, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "days old";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 323);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WinRotate";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button11;
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WinRotate/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinRotate
+{
+ 
+    public partial class Form1 : Form
+    {
+        
+
+        public void Log(string s)
+        {
+            textBox3.AppendText("["+Convert.ToString(DateTime.Now)+"] "+ s + "\n");
+        }
+
+        public string[] textbox2list;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox2.AppendText(folderBrowserDialog1.SelectedPath+System.Environment.NewLine);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {       // заполняем список папками из тексбокса
+                textbox2list = new string[textBox2.Lines.Length];
+                textBox2.ReadOnly = true;
+                string sum = "";
+                Log("Starting");
+
+               
+                if (textBox2.Lines.Length < 1000)
+                {
+                    for (int i = 0; i < textBox2.Lines.Length; i++)
+                    {
+                        textbox2list[i] = textBox2.Lines[i];
+                        sum += " " + textbox2list[i];
+                    }
+
+                    string textres = Convert.ToString(textBox2.Lines.Length);
+                    //System.Windows.Forms.MessageBox.Show("Added " + sum + " strings!", "Info",
+                    //System.Windows.Forms.MessageBoxButtons.OK,
+                    //System.Windows.Forms.MessageBoxIcon.Information);
+
+                }
+                else System.Windows.Forms.MessageBox.Show("More than we can handle", "Error",
+                           System.Windows.Forms.MessageBoxButtons.OK,
+                           System.Windows.Forms.MessageBoxIcon.Information);
+
+                // перебираем в цикле папки
+
+                for (int i = 0; i < textBox2.Lines.Length; i++)
+                {
+                    toolStripProgressBar1.Maximum = textBox2.Lines.Length;
+                    
+                    if (!String.IsNullOrEmpty(textbox2list[i]))
+                    {
+                       Log("Working on folder " + textbox2list[i]);
+
+                        string[] filepaths = System.IO.Directory.GetFiles(textbox2list[i]);
+                        toolStripProgressBar1.Value = i;
+
+                        for (int j = 0; j < filepaths.Length; j++)
+                        {
+                            if (System.IO.File.Exists(filepaths[j]) && (System.IO.File.GetCreationTime(filepaths[j])) < dateTimePicker1.Value)
+                            {
+                                try
+                                {
+                                    // удаляем файл
+                                    Log("Trying to delete " + filepaths[j]);
+                                    System.IO.File.Delete(filepaths[j]);
+                                    // count++;
+                                    // toolStripProgressBar1.Value = i;
+                                    // label1.Text = "Deleting " + DelFile + " now!";
+                                   Log("Deleted " + filepaths[j]);
+                                }
+                                catch (Exception ex)
+                                {
+                                    System.Windows.Forms.MessageBox.Show(ex.Message + " Problem:" + filepaths[j], "Error",
+                                    System.Windows.Forms.MessageBoxButtons.OK,
+                                    System.Windows.Forms.MessageBoxIcon.Warning);
+                                    // 
+                                }
+                                
+                            }
+                        }
+                    }
+                    //------------------------ перебор внутри папки ------------------------------
+                   
+                    
+                    // ----------------------- end of folder ---------------------------------
+                }
+                textBox2.ReadOnly = false;
+                toolStripProgressBar1.Value = 0;
+                Log("Done!");
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message + " Path:", "Error",
+                System.Windows.Forms.MessageBoxButtons.OK,
+                System.Windows.Forms.MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            Log("Cleared the box!");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today.AddDays(-30);
+            Log("Set date to 30 days ago!");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today.AddDays(-7);
+            Log("Set date to 7 days ago!");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today.AddDays(-5);
+            Log("Set date to 5 days ago!");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today.AddDays(-3);
+            Log("Set date to 3 days ago!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today.AddDays(-2); 
+            Log("Set date to 2 days ago!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today.AddDays(-1);
+            Log("Set date to 1 days ago!");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Today;
+        }
+
+        public void StartTimer()
+        {
+            timer1.Start();
+            textBox2.ReadOnly = true;
+            Log("Timer started for " + comboBox1.SelectedText + " and will ring at " + Convert.ToString(DateTime.Now.AddMilliseconds(timer1.Interval)));
+            button11.Text = "Stop job!";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Log("Trying to start job...");
+            if (timer1.Enabled == false)
+            {
+                Log("Checking if the timer is enabled: false ");
+                
+                    // 1000 = 1s
+                    // 1h = 1s*60*60 = 3600000
+                    // 1d = 1h*24 = 86400000
+                    // 2d = 172800000
+                    // 5d = 432000000
+                    // 7d = 604800000
+                    // 30d = 2592000000
+                //Log(Convert.ToString(comboBox1.SelectedIndex));
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 1: timer1.Interval = 3600000; StartTimer(); break;
+                    case 2: timer1.Interval = 86400000; StartTimer(); break;
+                    case 3: timer1.Interval = 172800000; StartTimer(); break;
+                    case 4: timer1.Interval = 432000000; StartTimer(); break;
+                    case 5: timer1.Interval = 604800000; StartTimer(); break;
+                    default: Log("No entry was selected! ");
+                        break;
+                }
+                          
+                
+            }
+            else
+            {
+                button11.Text = "Run this every";
+                textBox2.ReadOnly = false;
+                timer1.Stop();
+                Log("Job stoped by user!");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Now.AddDays(-Convert.ToInt32(numericUpDown1.Value));
+            button2.PerformClick();
+            textBox2.ReadOnly = false;
+            button11.Text = "Run this every";
+            timer1.Enabled = false;
+            Log("It is time! Job is done!");
+            if (checkBox1.Checked)
+            {
+                button11.PerformClick();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Lines.Length > 4000)
+            {
+                textBox2.Clear();
+            }
+        }
+
+       
+       }
+}
